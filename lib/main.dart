d9d4406e92ccf2edc84d097ba45278eeeca72ba0
Maria-Clara-Bertoli <mariaclarabertoli@outlook.com>
2022-12-13 22:48:20 -0300

    Aplicativo Cerveja Social
    Implementação do aplicativo Cerveja Social

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
 import 'dart:convert';
 import 'package:brasil/cerveja.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 import 'api.dart';
-import 'cerveja.dart';
 import 'cadastro.dart';
-import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+//import 'package:snapshot/snapshot.dart';
 
-void main() async{
-  await Hive.initFlutter();
+void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding : widgetsBinding);
   runApp(applicationBrasil());
 }
 
@@ -20,6 +21,7 @@ class applicationBrasil extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    FlutterNativeSplash.remove();
     return MaterialApp(
       home: implementation()
       );
@@ -28,6 +30,10 @@ class applicationBrasil extends StatelessWidget {
 
 class implementation extends StatefulWidget {
   implementation({Key? key}) : super(key: key);
+
+  initState() async{
+    await Hive.initFlutter();
+  }
 
   @override 
   _implementationState createState() => _implementationState();
@@ -52,53 +58,9 @@ class _implementationState extends State<implementation>{
     return;
   }
 
-  int countimagem = 0;
-  int countdescricao = 0;
-
-  void putDataImagem(){
-    boximagem!.put(cadastro.getidImagem(), cadastro.getImagem());
-  }
-
-   void putDataDescricao(){
-    boxdescricao!.put(cadastro.getidDescicao(), cadastro.getDescricao());
-  }
-
-  getDataImagem(int valor){
-    return boximagem!.get(valor);
-  }
-
-  getDataDescricao(int valor){
-    return boxdescricao!.get(valor);
-  }
-
-  void updateDataImagem(int valor, String descricao){
-    boximagem!.put(valor, descricao);
-  }
-
-   void updateDataDescricao(int valor, String descricao){
-   boxdescricao!.put(valor, descricao);
-  }
-
-  void deleteDataImagem(int valor){
-    boximagem!.delete(valor);
-  }
-
-  void deleteDataDescricao(int valor){
-    boxdescricao!.delete(valor);
-  }
-
-  getAllImagem(){
-    return (boximagem!.toMap());
-  }
-
-  getAllDescricao(){
-    return (boxdescricao!.toMap());
-  }
-
   Cadastro cadastro = new Cadastro();
 
   final TextEditingController _controllerimagem = TextEditingController();
-  final TextEditingController _controllerlike = TextEditingController();
   final TextEditingController _controllerdescricao = TextEditingController();
 
   List <Cerveja> cerveja = [];
@@ -123,44 +85,96 @@ class _implementationState extends State<implementation>{
       primarySwatch: Colors.amber,
     ),
     home: DefaultTabController(
-      length: 2,
+      length: 3,
       child: Scaffold(
         appBar: AppBar(
           title: Text(""),
           bottom: TabBar(tabs: [
-            Tab(icon: Text("LOCALIZAÇÃO", style: TextStyle(fontSize: 16, color: Colors.white))),
-            Tab(icon: Text("PUBLICAÇÃO", style: TextStyle(fontSize: 16, color: Colors.white))),
+            Tab(icon: Text("LOCALIZAÇÃO", 
+            textDirection: TextDirection.ltr,
+            style: TextStyle (fontSize: 13, fontWeight: FontWeight.w400, color: Colors.black))),
+            Tab(icon: Text("PUBLICAÇÃO", 
+            textDirection: TextDirection.ltr,
+            style: TextStyle (fontSize: 13, fontWeight: FontWeight.w400, color: Colors.black))),
+            Tab(icon: Text("VISUALIZAÇÃO", 
+            textDirection: TextDirection.ltr,
+            style: TextStyle (fontSize: 13, fontWeight: FontWeight.w400, color: Colors.black))),
             ]
           )
         ),
         body: TabBarView(children: [
-          ListView.builder(
-          itemCount: 16,
-          itemBuilder: (context, index){
-          return ListTile(
-          title: Text(
-          cerveja[index].name!,
-          textDirection: TextDirection.ltr,
-          style: TextStyle (fontSize: 20, fontWeight: FontWeight.w400, color: Colors.black)
+            Scaffold(
+              body: FutureBuilder(
+                future: Api.getCerveja(),
+                builder: (BuildContext context, AsyncSnapshot snapshot){
+                  if(snapshot.connectionState == ConnectionState.done){
+                    if(snapshot.hasData){
+                      return ListView.builder(
+                        itemCount: 16,
+                        itemBuilder: (context, index){
+                          return ListTile(
+                          title: Text(
+                          cerveja[index].name!,
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle (fontSize: 20, fontWeight: FontWeight.w400, color: Colors.black)
+                          ),
+                          subtitle: Text(
+                          cerveja[index].state!  + " - " + cerveja[index].city! + " - " + cerveja[index].street!,   
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle (fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black)
+                          )
+                        );
+                      }
+                    );
+                  }
+                  else {
+                    return Center(
+                      child: const Text("Desculpe! Houve um erro de conexão.", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w300, color: Colors.red)),
+                    );
+                  }
+                }
+                else if(snapshot.connectionState == ConnectionState.waiting){
+                  return Row(
+                   mainAxisSize: MainAxisSize.max,
+                   mainAxisAlignment: MainAxisAlignment.center, 
+                   children:[
+                    Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [const CircularProgressIndicator()]
+                    )
+                   ]
+                  );
+                }
+                else {
+                  return Center(
+                    child: const Text("Desculpe! Houve um erro de conexão.", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400, color: Colors.red)),
+                  );
+                }
+              }
+            )
           ),
-          subtitle: Text(
-          cerveja[index].state!  + " - " + cerveja[index].city! + " - " + cerveja[index].street!,   
-          textDirection: TextDirection.ltr,
-          style: TextStyle (fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black)
-          )
-        );
-      },
-    ),
 
-          Column(children: [
-           Image.asset("assets/images/cerveja.png", width: 400, height: 300), 
+        Container(decoration: new BoxDecoration(image: new DecorationImage(image: new AssetImage("assets/images/fundo.jpg"),
+        fit: BoxFit.cover)),
+          child: Column(children: [ 
            Padding(
               padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
                 child: TextField(
                   controller: _controllerimagem,
-                  decoration: InputDecoration(hintText: "Informe a url de uma imagem", 
-                  border: OutlineInputBorder(
+                  style: TextStyle(
+                    color: Colors.amber,
                   ),
+                  decoration: InputDecoration(hintText: "Informe a 'URL' de uma imagem",
+                  hintStyle: TextStyle(
+                    color: Colors.amber
+                  ), 
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 2,
+                        color: Colors.amber
+                      )
+                    ),
                   ),
                 ),
               ),
@@ -168,21 +182,31 @@ class _implementationState extends State<implementation>{
               padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
                 child: TextField(
                   controller: _controllerdescricao,
-                  decoration: InputDecoration(hintText: "Informe uma descrição", 
-                  border: OutlineInputBorder(
+                  style: TextStyle(
+                    color: Colors.amber,
                   ),
+                  decoration: InputDecoration(hintText: "Informe a descrição",
+                  hintStyle: TextStyle(
+                    color: Colors.amber
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        width: 2,
+                        color: Colors.amber
+                      )
+                    ),
                   ),
                 ),
               ),
               ElevatedButton(
                 child: Text('PUBLICAR',
                 textDirection: TextDirection.ltr,
-                style: TextStyle(fontSize: 17),
+                style: TextStyle(fontSize: 15),
                 ),
                 style: ElevatedButton.styleFrom(
                   minimumSize: Size(350, 40),
                   primary: Colors.amber,
-                  onPrimary: Colors.white,
+                  onPrimary: Colors.black,
                 ),  
                 onPressed: (){
                   setState(() {
@@ -194,16 +218,53 @@ class _implementationState extends State<implementation>{
 
                     cadastro.setImagem(imagem!);
                     cadastro.setDescricao(descricao!);
-                    cadastro.setidImagem(cadastro.getidImagem() + 1);
-                    cadastro.setidDescricao(cadastro.getidDescicao() + 1);
-                    putDataDescricao();
+                    boximagem!.add(cadastro.getImagem());
+                    boxdescricao!.add(cadastro.getDescricao());
                     }
                   );
                 },
               ),
             ],
           )
-        ],
+         ),        
+                      ListView.builder(
+                      itemCount: boxdescricao!.length,
+                      itemBuilder: (context, index){
+                      return ListTile(
+                        title: Card(
+                          color: Colors.grey[198],
+                          child: Container(
+                          padding: EdgeInsets.all(5),
+                          child: Column(children: [
+                              Image.network(boximagem!.getAt(index), fit: BoxFit.fitWidth, alignment: Alignment.bottomCenter, width: double.infinity, height: 240,),
+                              Divider(),
+                              Text (boxdescricao!.getAt(index), 
+                              textDirection: TextDirection.ltr,
+                              style: TextStyle (fontSize: 19, fontWeight: FontWeight.w400, color: Colors.black),
+                              ),
+                            Row(children: [
+                              Text("                                                                                    "),
+                              IconButton(onPressed:(){
+                                boximagem!.deleteAt(index);
+                                boxdescricao!.deleteAt(index);
+                                setState(() {
+                                  boxdescricao!.length;
+                                });
+                              },
+                              icon: Icon(Icons.close_sharp, color: Colors.amber),
+                              color: Colors.red,
+                            )
+                          ],
+                        )
+                              
+                          ],
+                        )
+                      ) 
+                    ), 
+                  );
+                }
+              )
+            ],
           )
         )
       )
